fda2ef99b7335c2585123abe0076e386a9f0d15c
sumit <sumitskl1999@gmail.com>
2019-10-12 18:50:35 +0530

    ..

--- routes/paths.js
+++ routes/paths.js
@@ -3,5 +3,6 @@ const user = require('../middlewares/user');
 
 // Payments Paths...
 router.get('/api/get-user', (req, res) => user.fetchuser(req,res));
+router.get('/', (req, res) => res.send("welcome to sumit's api"));
 
 module.exports = router;
